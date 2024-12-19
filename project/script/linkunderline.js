b0dda6a65725cb56e92c8f0cc2c3818c22c9d7e7
Kormilicyna-Dasha <kormilicynadasha@gmail.com>
2024-12-19 21:51:04 +0300

    Add files via upload

---
+++ project/script/linkunderline.js
@@ -0,0 +1,29 @@
+/*
+document.addEventListener('DOMContentLoaded', () => {
+    const links = document.querySelectorAll('.nav-link'); // Ссылки навигации
+    const currentUrl = window.location.pathname; // Получение текущего пути (к примеру, /about.html)
+
+    links.forEach(link => {
+        // Если href ссылки совпадает с текущим путем
+        if (link.getAttribute('href') === currentUrl) {
+            link.classList.add('underlined-link'); // Добавляем класс для подсветки
+            link.classList.add('.nav-link:after');
+        }
+
+    });
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+    const links = document.querySelectorAll('.down-links'); // Ссылки навигации
+    const currentUrl = window.location.pathname; // Получение текущего пути (к примеру, /about.html)
+
+    links.forEach(link => {
+        // Если href ссылки совпадает с текущим путем
+        if (link.getAttribute('href') === currentUrl) {
+            link.classList.add('changecolor-link'); // Добавляем класс для подсветки
+            
+        }
+
+    });
+});
+*/
